d14abee2e3cc5db81d3227c5764f20722890798a
mfaizudd <mfaizudd@gmail.com>
2021-10-07 21:19:03 +0700

    feat: Heading component

---
+++ components/Headings.tsx
@@ -0,0 +1,17 @@
+export const Heading1: React.FC = ({children}) => {
+    return (
+        <h1 className="text-2xl">{children}</h1>
+    )
+}
+
+export const Heading2: React.FC = ({children}) => {
+    return (
+        <h1 className="text-xl">{children}</h1>
+    )
+}
+
+export const Heading3: React.FC = ({children}) => {
+    return (
+        <h1 className="text-l">{children}</h1>
+    )
+}
