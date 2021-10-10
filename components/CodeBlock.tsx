9e571feaa212b318e568d306744e6601587d36ba
mfaizudd <mfaizudd@gmail.com>
2021-10-10 17:37:12 +0700

    fix: update code block, inline code

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -4,13 +4,32 @@ import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
 // import SyntaxHighlighter from "react-syntax-highlighter";
 // import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
-const CodeBlock: CodeComponent = ({className, children}) => {
+const CodeBlock: CodeComponent = ({ className, children, inline }) => {
     const match = /language-(\w+)/.exec(className ?? "");
     const lang = match ? match[1] : "auto";
-    return (
-        <SyntaxHighlighter language={lang} style={atomDark}>
+    let block = (
+        <SyntaxHighlighter language={lang} style={atomDark} showLineNumbers={true}>
             {children}
         </SyntaxHighlighter>
+    );
+    if (inline) {
+        block = (
+            <span
+                style={
+                    { 
+                        backgroundColor: "#1d1f21",
+                        paddingTop: "0.1rem",
+                        paddingBottom: "0.1rem"
+                    }
+                }
+                className="text-gray-300 font-mono px-1 rounded-md">
+                    
+                {children}
+            </span>
+        )
+    }
+    return (
+        block
     )
 }
 
