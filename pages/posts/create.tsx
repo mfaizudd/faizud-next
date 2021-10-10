183eb4741285a3b15ea24db067bbcb8f1b622cfe
mfaizudd <mfaizudd@gmail.com>
2021-10-10 17:38:08 +0700

    fix: update markdown editor

--- pages/posts/create.tsx
+++ pages/posts/create.tsx
@@ -6,13 +6,16 @@ import Router from "next/router";
 import Submit from "components/Submit";
 import TextArea from "components/TextArea";
 import Link from 'next/link';
-// import Editor from "components/Editor";
 import dynamic from "next/dynamic";
-const Editor = dynamic(() => import('components/Editor'), {ssr: false});
-// const MDEditor = dynamic(
-//   () => import("@uiw/react-md-editor").then((mod) => mod.default),
-//   { ssr: false }
-// );
+import rehypeHighlight from 'rehype-highlight';
+import 'react-markdown-editor-lite/lib/index.css';
+import ReactMarkdown from "react-markdown";
+import CodeBlock from "components/CodeBlock";
+import { Heading1, Heading2, Heading3 } from "components/Headings";
+
+const MdEditor = dynamic(() => import('react-markdown-editor-lite'), {
+    ssr: false,
+});
 
 const Draft: React.FC = () => {
     const [title, setTitle] = useState('');
@@ -33,18 +36,32 @@ const Draft: React.FC = () => {
         }
     }
 
+    const render = (text: string) => {
+        return (
+            <ReactMarkdown plugins={[rehypeHighlight]} components={{
+                code: CodeBlock,
+                h1: Heading1,
+                h2: Heading2,
+                h3: Heading3
+            }}>{text}</ReactMarkdown>
+        )
+    }
+
     return (
         <Layout>
             <h1 className="mx-5 text-4xl font-bold">New Draft</h1>
             <Form onSubmit={onSubmit} method="post">
-                <InputText value={title} onChange={e=>setTitle(e.target.value)} name="title"/>
-                {(typeof window !== undefined) && (
-                <Editor 
-                    value={content} 
-                    onChange={setContent} />
-                )}
+                <InputText value={title} onChange={e => setTitle(e.target.value)} name="title" />
+                <div className="px-3 my-3 w-full">
+                    <MdEditor
+                        style={{ height: '500px' }}
+                        renderHTML={text => render(text)}
+                        value={content}
+                        onChange={({text}) => setContent(text)}
+                    />
+                </div>
                 <div className="px-3 my-3">
-                    <Submit label="Create"/>
+                    <Submit label="Create" />
                     <Link href="/posts">
                         <a> or Cancel</a>
                     </Link>
