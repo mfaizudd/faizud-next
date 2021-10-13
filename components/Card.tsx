f5035eb8d3b2d99fd7fee459fefa1adca8bc72e1
mfaizudd <mfaizudd@gmail.com>
2021-10-13 12:47:26 +0700

    fix: remove post preview

--- components/Card.tsx
+++ components/Card.tsx
@@ -12,18 +12,17 @@ interface CardProps {
 const Card: React.FC<CardProps> = (props) => {
     return (
         <div className="bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl hover:shadow-2xl transition-shadow flex-grow">
-            <div className="md:flex">
+            <div className="md:flex h-full">
                 <div className="md:flex-shrink-0">
                     <div className="h-48 w-full object-cover md:h-full md:w-48 relative">
-                        <Image src={props.image ?? "/favicon.ico"} layout="fill" objectFit="cover" alt="Man looking at item at a store"/>
+                        <Image src={props.image ?? "/favicon.ico"} layout="fill" objectFit="cover" alt="Man looking at item at a store" />
                     </div>
                 </div>
-                <div className="p-8">
-                <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{ props.category }</div>
-                <Link href={props.route ?? "#"}>
-                    <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{ props.title }</a>
-                </Link>
-                <p className="mt-2 text-gray-500">{ props.description }</p>
+                <div className="p-8 py-16">
+                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{props.category}</div>
+                    <Link href={props.route ?? "#"}>
+                        <a className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{props.title}</a>
+                    </Link>
                 </div>
             </div>
         </div>
