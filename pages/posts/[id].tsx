c66eac9fc1263fd3d30cb806d4c8043aefcff478
mfaizudd <mfaizudd@gmail.com>
2021-10-14 12:37:38 +0700

    fix: update post view

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,4 +1,4 @@
-import { Post } from '.prisma/client';
+import { Post, User } from '.prisma/client';
 import Layout from 'components/Layout';
 import { GetServerSideProps } from 'next';
 import prisma from 'lib/prisma'
@@ -6,12 +6,14 @@ import Router from 'next/router';
 import ReactMarkdown from 'react-markdown';
 import rehypeHighlight from 'rehype-highlight';
 import CodeBlock from 'components/CodeBlock';
+import Error from 'next/error';
 import { useSession } from 'next-auth/client';
-import { CheckCircle } from 'react-feather';
+import { Check } from 'react-feather';
 import { Heading1, Heading2, Heading3 } from 'components/Headings';
+import FloatingButton from 'components/FloatingButton';
 
 interface PostProps {
-    post: Post & { author: { name: string | null} }
+    post: Post & { author: User }
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ params }) => {
@@ -20,9 +22,7 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
             id: Number(params?.id) || -1
         },
         include: {
-            author: {
-                select: { name: true }
-            }
+            author: true
         }
     });
 
@@ -33,20 +33,32 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     };
 }
 
-const Show: React.FC<PostProps> = ({post}) => {
+const Show: React.FC<PostProps> = ({ post }) => {
+    const [session, loading] = useSession();
+    if (loading) {
+        return <div>Authenticating...</div>;
+    }
+    if (post == null) {
+        return <Error statusCode={404} />;
+    }
+    const loggedIn = Boolean(session);
+    const unpublished = post.published === false;
+    const owned = session?.user?.email === post.author.email;
+
     const publishPost = async (id: number) => {
         await fetch(`/api/posts/${id}/publish`, {
             method: 'PUT',
         });
         await Router.push('/posts');
     }
+
     return (
         <Layout>
             <div className="max-w-3xl mx-auto my-4 lg:w-3/4">
                 <h1 className="ml-3 text-4xl">{post.title}</h1>
                 <sub className="text-gray-600 w-full inline-block text-right">By {post.author.name}</sub>
-                <hr/>
-                <br/>
+                <hr />
+                <br />
                 <ReactMarkdown className="space-y-5 text-justify" plugins={[rehypeHighlight]} components={{
                     code: CodeBlock,
                     h1: Heading1,
@@ -55,8 +67,12 @@ const Show: React.FC<PostProps> = ({post}) => {
                 }}>
                     {post.content ?? ""}
                 </ReactMarkdown>
-                <div className="fixed rounded-full bg-gray-800 right-4 bottom-4 p-3 cursor-pointer" onClick={e => publishPost(post.id)}>
-                    <CheckCircle className="text-white"/>
+                <div className="fixed bottom-5 right-5 flex flex-row">
+                    {loggedIn && unpublished && owned && (
+                        <FloatingButton onClick={_e => publishPost(post.id)} title="Publish">
+                            <Check className="text-white" />
+                        </FloatingButton>
+                    )}
                 </div>
             </div>
         </Layout>
