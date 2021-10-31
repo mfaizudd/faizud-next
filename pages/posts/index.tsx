a8f4fd88536230469b7380e6f8ed36297270a0da
mfaizudd <mfaizudd@gmail.com>
2021-10-31 14:55:49 +0700

    fix: update post drafts and index

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,19 +1,7 @@
 import type { GetServerSideProps, NextPage } from 'next'
-import Link from 'next/link';
-import Layout from 'components/Layout'
 import prisma from 'lib/prisma';
-import { GetStaticProps } from 'next';
-import Card from 'components/Card';
 import { Category, Post, User } from '.prisma/client';
-import { getSession, useSession } from 'next-auth/client';
-import PostList from 'components/PostList';
-import { useState } from 'react';
-import FloatingButton from 'components/FloatingButton';
-import { FilePlus } from 'react-feather';
-import { Session } from 'next-auth';
-import Confirm, { ConfirmType } from 'components/Confirm';
-import Router from 'next/router';
-import axios, { AxiosError } from 'axios';
+import PostsPage from 'components/PostsPage';
 
 type PostItem = Post & { author: User, category: Category }
 
@@ -46,130 +34,8 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     }
 }
 
-const Posts: NextPage<PostsProps> = (props) => {
-    const [session, loading] = useSession();
-    let loadingElement = null;
-    let createElement = null;
-    if (loading) {
-        loadingElement = (
-            <div className="mx-auto">
-                Loading...
-            </div>
-        )
-    }
-    if (session) {
-        createElement = (
-            <Link href="/posts/create" passHref>
-                <div className="fixed right-5 bottom-5">
-                    <FloatingButton title="Create">
-                        <FilePlus className="text-white" />
-                    </FloatingButton>
-                </div>
-            </Link>
-        )
-    }
-
-    const [posts, setPosts] = useState(props.posts)
-
-    // selected post to delete/publish
-    const [post, setPost] = useState<Post | null>(null)
-    const [loadingPost, setLoadingPost] = useState(false);
-    const [hasMore, setHasMore] = useState(posts.length < props.totalPost);
-    const [isConfirmDelete, setIsConfirmDelete] = useState(false);
-    const [isConfirmPublish, setIsConfirmPublish] = useState(false);
-
-    const onPublish = (post: Post) => {
-        setIsConfirmPublish(true);
-        setPost(post);
-    }
-
-    const onDelete = (post: Post) => {
-        setIsConfirmDelete(true);
-        setPost(post);
-    }
-
-    const deletePost = async (id?: number) => {
-        if (!id) return;
-
-        try {
-            const response = await axios.delete(`/api/posts/${id}/delete`);
-            if (response.status === 200) {
-                // Refresh posts
-            }
-        } catch (error: any) {
-            console.error(error.response.data);
-        }
-    }
-
-    const publish = async (id?: number) => {
-        if (!id) return;
-
-        try {
-            const response = await axios.put(`/api/posts/${id}/publish`);
-            if (response.status === 200) {
-                // TODO: Refresh posts
-            }
-        } catch (error: any) {
-            console.error(error.response.data);
-        }
-    }
-
-    const getMorePosts = async () => {
-        setLoadingPost(true);
-        try {
-            const response = await axios.get(`/api/posts?take=3&skip=${posts.length}&published=true`, {
-                headers: { 'Content-Type': 'application/json' }
-            });
-            if (response.status === 200) {
-                const data = response.data;
-                setPosts([...posts, ...data.posts]);
-                setHasMore(posts.length + data.posts.length < data.total);
-            }
-        } catch (error: any) {
-            console.error(error.response.data);
-        }
-        setLoadingPost(false);
-    }
-
-    return (
-        <Layout title="Posts">
-            <div className="flex flex-col gap-2 m-4 justify-evenly flex-grow">
-                {loadingElement}
-                <PostList
-                    posts={posts}
-                    onDelete={onDelete}
-                    onPublish={onPublish} />
-                {loadingPost && loadingElement}
-            </div>
-            {createElement}
-            {hasMore && (
-                <div className="text-center text-white bg-blue-500 rounded-lg p-2 cursor-pointer m-2" onClick={getMorePosts}>
-                    More Posts
-                </div>
-            )}
-            <div className="text-center">
-                <Link href="/">Back</Link>
-            </div>
-            <Confirm
-                title="Confirm delete"
-                desc="Are you sure you want to delete this post?"
-                confirmType={ConfirmType.Danger}
-                isOpen={isConfirmDelete}
-                onConfirm={() => deletePost(post?.id)}
-                onCancel={() => setIsConfirmDelete(false)}
-                onClose={() => setIsConfirmDelete(false)}
-            />
-            <Confirm
-                title="Confirm publish"
-                desc="Are you sure you want to publish this post?"
-                confirmType={ConfirmType.Success}
-                isOpen={isConfirmPublish}
-                onConfirm={() => publish(post?.id)}
-                onCancel={() => setIsConfirmPublish(false)}
-                onClose={() => setIsConfirmPublish(false)}
-            />
-        </Layout>
-    )
+const PostsIndex: NextPage<PostsProps> = (props) => {
+    return <PostsPage {...props} published={true} />
 }
 
-export default Posts;
+export default PostsIndex;
