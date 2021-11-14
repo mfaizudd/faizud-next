823f6a691c58e5d5572d77291cb7d4c29582121d
mfaizudd <mfaizudd@gmail.com>
2021-11-14 21:04:29 +0700

    fix: 'update' duplicate slug

--- pages/api/posts/[id]/update.ts
+++ pages/api/posts/[id]/update.ts
@@ -11,6 +11,17 @@ const UpdatePost: NextApiHandler = async (req, res) => {
         return res.status(405).end("Method not allowed");
     }
     let { title, slug, categoryId, featuredImage, content } = body;
+    const existingPost = await prisma.post.findFirst({
+        where: {
+            slug: { contains: slug }
+        },
+        orderBy: { slug: 'desc' }
+    });
+    if (existingPost !== null) {
+        let lastNumber = Number(existingPost.slug.slice(slug.length));
+        let originalSlug = existingPost.slug.slice(0, slug.length);
+        slug = `${originalSlug}${lastNumber + 1}`
+    }
     const result = await prisma.post.update({
         where: { id: Number(id) },
         data: {
