9ecaba08b1106ebcf2c4883b799c315728e47430
mfaizudd <mfaizudd@gmail.com>
2021-11-28 20:59:07 +0700

    feat: progress on edit

--- pages/posts/[slug]/edit.tsx
+++ pages/posts/[slug]/edit.tsx
@@ -5,7 +5,8 @@ import { GetServerSideProps, NextPage } from "next";
 import prisma from "lib/prisma";
 import { Category, Post } from ".prisma/client";
 import axios from "axios";
-import PostForm from "components/Post/PostForm";
+import PostForm, { PostData } from "components/Post/PostForm";
+import { toast } from "react-toastify";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
     const categories = await prisma.category.findMany();
@@ -26,23 +27,31 @@ interface EditProps {
 }
 
 const Edit: NextPage<EditProps> = ({ post, categories }) => {
+    const onSubmit = async (data: PostData) => {
+        const toastId = toast.loading("Updating...", { theme: "dark" })
+        try {
+            await axios.put(`/api/posts/${post.id}/update`, data);
+            toast.update(toastId, {
+                render: "Updated",
+                type: "success",
+                isLoading: false,
+                autoClose: 5000
+            });
+            if (post.published) {
+                await Router.push('/posts');
+            }
+            else {
+                await Router.push('/posts/drafts');
+            }
+        } catch (error: any) {
+            console.error(error);
+        }
+    }
     return (
         <PostForm
             post={post}
             categories={categories}
-            onSubmit={async data => {
-                try {
-                    await axios.put(`/api/posts/${post.id}/update`, data);
-                    if (post.published) {
-                        await Router.push('/posts');
-                    }
-                    else {
-                        await Router.push('/posts/drafts');
-                    }
-                } catch (error:any) {
-                    console.error(error);
-                }
-            }} />
+            onSubmit={onSubmit} />
     )
 }
 
