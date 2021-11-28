6bc086ea262b0f578e2105637efa06e32dfc8bbf
mfaizudd <mfaizudd@gmail.com>
2021-11-28 17:23:27 +0700

    fix(artwork card): clickable area

--- components/Artwork/Card.tsx
+++ components/Artwork/Card.tsx
@@ -18,12 +18,14 @@ const Card: React.FC<CardProps> = (props) => {
                         <Image src={props.image || "/enbandeng.png"} layout="fill" objectFit="cover" alt="Man looking at item at a store" />
                     </div>
                 </div>
-                <div className="absolute p-8 py-8 left-0 top-0 md:opacity-0 hover:opacity-100 w-full h-1/2 transition-all bg-gradient-to-b from-black to-transparent">
-                    <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{props.category}</div>
-                    <Link href={props.route ?? "#"}>
-                        <a className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">{props.title}</a>
-                    </Link>
-                </div>
+                <Link href={props.route ?? "#"} passHref={true}>
+                    <div className="absolute p-8 py-8 left-0 top-0 md:opacity-0 hover:opacity-100 w-full h-full transition-all bg-gradient-to-b from-black to-transparent cursor-pointer">
+                        <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{props.category}</div>
+                        <Link href={props.route ?? "#"}>
+                            <a className="block mt-1 text-lg leading-tight font-medium text-white hover:underline">{props.title}</a>
+                        </Link>
+                    </div>
+                </Link>
             </div>
             {props.children}
         </div>
