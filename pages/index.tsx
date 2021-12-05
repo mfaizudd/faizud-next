946dec16e204f5b2b7940b33e748185c1815b708
mfaizudd <mfaizudd@gmail.com>
2021-12-05 21:17:33 +0700

    fix(homepage): delete post

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,10 @@ import prisma from 'lib/prisma';
 import PostList from 'components/Post/PostList';
 import { getSession } from 'next-auth/client';
 import ArtworkList from 'components/Artwork/ArtworkList';
-import Image from 'next/image';
-import { url } from 'inspector';
+import Confirm, { ConfirmType } from 'components/Confirm';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import axios, { AxiosResponse } from 'axios';
 
 type PostWithAuthor = Post & { author: User }
 interface PostsProps {
@@ -72,7 +74,51 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 
-const Home: NextPage<PostsProps> = ({ posts, artworks, loggedInUser }) => {
+const Home: NextPage<PostsProps> = (props) => {
+    const [isConfirm, setIsConfirm] = useState(false);
+    const [post, setPost] = useState<Post>();
+    const [posts, setPosts] = useState(props.posts);
+    const deletePost = (post: Post) => {
+        setPost(post);
+        setIsConfirm(true);
+    }
+    const executeDelete = async () => {
+        setIsConfirm(false);
+        const toastId = toast.loading("Processing...", { theme: "dark" });
+        try {
+            const response = await axios.delete(`/api/posts/${post?.id}/delete`);
+            if (response?.status === 200) {
+                await refreshPosts();
+                // TODO: refresh artworks
+            }
+            setPost(undefined);
+        } catch (error: any) {
+            toast.update(toastId, {
+                render: `${error.response.status}: ${error.response.data}`,
+                type: "error",
+                isLoading: false,
+                autoClose: 5000,
+                closeButton: true
+            });
+        }
+        toast.update(toastId, {
+            render: "Post deleted",
+            type: "success",
+            isLoading: false,
+            autoClose: 5000
+        });
+    }
+    const refreshPosts = async () => {
+        try {
+            const response = await axios.get(`/api/posts?take=${posts.length}&skip=0&published=true`);
+            if (response.status === 200) {
+                const data = response.data;
+                setPosts(data.posts);
+            }
+        } catch (error: any) {
+            console.error(error.response.data);
+        }
+    }
     return (
         <Layout title="Index">
             <div className="flex flex-col justify-center items-center h-24 w-full">
@@ -86,12 +132,24 @@ const Home: NextPage<PostsProps> = ({ posts, artworks, loggedInUser }) => {
             </div>
             <div className="flex lg:flex-row m-5 flex-col items-center gap-4">
                 <div className="w-full lg:w-7/12 md:mx-auto flex flex-col md:flex-row gap-2 justify-evenly flex-wrap">
-                    <ArtworkList posts={artworks} loggedInUser={loggedInUser} />
+                    <ArtworkList posts={props.artworks} loggedInUser={props.loggedInUser} />
                 </div>
                 <div className="w-full lg:w-5/12 md:mx-auto flex flex-col gap-2 justify-evenly">
-                    <PostList posts={posts} loggedInUser={loggedInUser} />
+                    <PostList
+                        posts={posts}
+                        loggedInUser={props.loggedInUser}
+                        onDelete={post => deletePost(post)} />
                 </div>
             </div>
+            <Confirm
+                title="Are you sure?"
+                desc="Are you sure you want to delete this post?"
+                confirmType={ConfirmType.Danger}
+                isOpen={isConfirm}
+                onConfirm={() => executeDelete()}
+                onCancel={() => setIsConfirm(false)}
+                onClose={() => setIsConfirm(false)}
+            />
         </Layout>
     )
 }
